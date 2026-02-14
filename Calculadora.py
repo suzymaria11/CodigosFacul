4acfd15c8697db9b41a04852eca4c424e89bc616
suzy.maria11 <suzy.maria11@aluno.ifce.edu.br>
2026-02-14 14:52:22 -0300

    Adiciona um loop para fazer mais de uma operação e uma opção para sair

--- Calculadora.py
+++ Calculadora.py
@@ -1,23 +1,35 @@
-operacao = input('''Digite um dos símbolos a seguir: +,-,/,*,% ou ^ (para potenciação)
-Qual operação vai ser realizada? ''')
 
-num1 = int(input("Digite o primeiro número: "))
-num2 = int(input("Digite o segundo número: "))
+while True:
+    operacao = input('''Digite um dos símbolos a seguir: +,-,/,*,% ou ^ (para potenciação)
+Qual operação vai ser realizada? (ou 'sair' para encerrar) ''')
+    
+    if operacao.lower() == 'sair':
+        print("Até mais")
+        break
 
-if operacao not in ['+', '-', '*', '/', '%', '^']:
-    print("Erro na operação. Digite novamente um dos símbolos a seguir: +,-,/,*,% ou ^ (para potenciação)")
-elif operacao == '+':
-    print(f"{num1} + {num2} = {num1 + num2}")
-elif operacao == '-':
-    print(f"{num1} - {num2} = {num1 - num2}")
-elif operacao == '*':
-    print(f"{num1} * {num2} = {num1 * num2}")
-elif operacao == '/':
-    if num2 != 0:
-        print(f"{num1} / {num2} = {num1 / num2}")
-    else:
-        print("Divisão por zero não é possível.")
-elif operacao == '%':
-    print(f"{num1} % {num2} = {num1 % num2}")
-elif operacao == '^':
-    print(f"{num1} ^ {num2} = {num1 ** num2}")
+    num1 = int(input("Digite o primeiro número: "))
+    num2 = int(input("Digite o segundo número: "))
+
+    if operacao not in ['+', '-', '*', '/', '%', '^']:
+        print("Erro na operação. Digite novamente um dos símbolos a seguir: +,-,/,*,% ou ^ (para potenciação)")
+    elif operacao == '+':
+        print(f"{num1} + {num2} = {num1 + num2}")
+    elif operacao == '-':
+        print(f"{num1} - {num2} = {num1 - num2}")
+    elif operacao == '*':
+        print(f"{num1} * {num2} = {num1 * num2}")
+    elif operacao == '/':
+        if num2 != 0:
+            print(f"{num1} / {num2} = {num1 / num2}")
+        else:
+            print("Divisão por zero não é possível.")
+    elif operacao == '%':
+        print(f"{num1} % {num2} = {num1 % num2}")
+    elif operacao == '^':
+        print(f"{num1} ^ {num2} = {num1 ** num2}")
+
+
+    continuar = input("Deseja fazer outra conta? (Sim ou Não): ")
+    if continuar.upper() != "Sim":
+        print("Operações encerradas!")
+        break
