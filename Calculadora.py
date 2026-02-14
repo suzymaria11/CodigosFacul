cefb6357d457d6971be74093dfd5b525e59540c9
suzy.maria11 <suzy.maria11@aluno.ifce.edu.br>
2026-02-14 15:36:03 -0300

    adiciona mais operações e mensagens de erro e uma condição para que fatorial e raiz quadrada funcionem já que eles só precisam de um número.

--- Calculadora.py
+++ Calculadora.py
@@ -1,14 +1,19 @@
+import math
 
 while True:
-    operacao = input('''Digite um dos símbolos a seguir: +,-,/,*,% ou ^ (para potenciação)
-Qual operação vai ser realizada? (ou 'sair' para encerrar) ''')
+    operacao = input('''Operações: 
+    + - adição          % - resto
+    - - subtração       ^ - potência 
+    / - divisão         r - raiz quadrada
+    * - multiplicação   f - fatorial               
+    Qual operação vai ser realizada (ou 'sair' para encerrar)? ''')
     
     if operacao.lower() == 'sair':
         print("Até mais")
         break
 
-    num1 = int(input("Digite o primeiro número: "))
-    num2 = int(input("Digite o segundo número: "))
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
 
     if operacao not in ['+', '-', '*', '/', '%', '^']:
         print("Erro na operação. Digite novamente um dos símbolos a seguir: +,-,/,*,% ou ^ (para potenciação)")
@@ -27,9 +32,13 @@ Qual operação vai ser realizada? (ou 'sair' para encerrar) ''')
         print(f"{num1} % {num2} = {num1 % num2}")
     elif operacao == '^':
         print(f"{num1} ^ {num2} = {num1 ** num2}")
+    elif operacao == 'r':
+        print(f"√{num1} = {math.sqrt(num1)}")
+    elif operacao == '!':
+        print(f"{int(num1)}! = {math.factorial(int(num1))}")
 
 
     continuar = input("Deseja fazer outra conta? (Sim ou Não): ")
     if continuar.upper() != "Sim":
-        print("Operações encerradas!")
+        print("Operações encerradas.")
         break
